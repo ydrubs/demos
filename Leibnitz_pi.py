71ceea3af3228806682d13f607a26c3a1c128e3a
Yuriy Drubinskiy <ydrubins@yahoo.com>
2023-09-13 22:26:10 -0500

    Another way to solve the problem

--- Leibnitz_pi.py
+++ Leibnitz_pi.py
@@ -13,8 +13,22 @@ for i in range(1, iterations+1):
 print(total)
 
 #Break it down into smaller problems
-# Problem 1: How can you loop through (and get the sum of) the odd numbers a given amount of iterations?
+# Problem 1: How can you loop through (and get the sum of) the odd numbers a given amount of iterations (for example for 3 iterations, take the sum of 1+3+5)?
 # Problem 2: How can you loop through (and get the sum of) the odd unit fractions a given amount of iterations?
 # Problem 3: How can you alternate between adding and subtracting consecutive terms (preferably within ONE loop)?
             #How can you get 1-3+5-7?
 
+
+#Strategy #2
+subt_part = 0
+add_part = 0
+for i in range(1,2*(iterations),4):
+    print(i)
+    add_part +=1/i
+
+for i in range(3,2*(iterations),4):
+    print(i)
+    subt_part -=1/i
+
+
+print(add_part+subt_part)
