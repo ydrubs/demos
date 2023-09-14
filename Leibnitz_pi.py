3fb1f2c9f195fd7c8920a36fb3e10b8495e81077
Yuriy Drubinskiy <ydrubins@yahoo.com>
2023-09-13 22:28:21 -0500

    fixed comments

--- Leibnitz_pi.py
+++ Leibnitz_pi.py
@@ -2,7 +2,6 @@
 iterations = int(input("How many iterations: "))
 total = 0
 
-#addition loop (1/1 + 1/5 + 1/9 + ....)
 for i in range(1, iterations+1):
     denominator = 2*i-1
     # print(denominator)
@@ -22,10 +21,13 @@ print(total)
 #Strategy #2
 subt_part = 0
 add_part = 0
+
+#addition loop (1/1 + 1/5 + 1/9 + ....)
 for i in range(1,2*(iterations),4):
     print(i)
     add_part +=1/i
 
+#subtraction loop (-1/3 - 1/7 - 1/11 - ....)
 for i in range(3,2*(iterations),4):
     print(i)
     subt_part -=1/i
